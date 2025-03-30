984819785f7eefa542016b749ed8950f4273b2c1
Sash Subramanian <sashwath.subramanian1@students.mq.edu.au>
2025-03-30 18:40:00 +1100

    Fixed App Java breaking

--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -56,10 +56,6 @@ public class App {
             String line;
             int i = 1;
             while ((line = br.readLine()) != null) {
-                if(line.matches("^[a-z]{4}$")){
-                    System.out.println(line);
-                    wordleDatabaseConnection.addValidWord(i, line); 
-                }
                 System.out.println(line);
                 wordleDatabaseConnection.addValidWord(i, line);
                 i++;
@@ -78,16 +74,12 @@ public class App {
             String guess = scanner.nextLine();
 
             while (!guess.equals("q")) {
-                if(guess.matches("^[a-z]{4}$")) {
-                    System.out.println("You've guessed '" + guess+"'.");
+                System.out.println("You've guessed '" + guess+"'.");
 
-                    if (wordleDatabaseConnection.isValidWord(guess)) { 
-                        System.out.println("Success! It is in the the list.\n");
-                    } else{
-                        System.out.println("Sorry. This word is NOT in the the list.\n");
-                    } 
-                } else {
-                    System.out.println("The word '" + guess + "' is not a valid word.");
+                if (wordleDatabaseConnection.isValidWord(guess)) { 
+                    System.out.println("Success! It is in the the list.\n");
+                }else{
+                    System.out.println("Sorry. This word is NOT in the the list.\n");
                 }
 
                 System.out.print("Enter a 4 letter word for a guess or q to quit: " );
